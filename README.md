1c1ca996834d0bfde65b4907444029a105561fdc
Benjamin Herr <ben@0x539.de>
2013-04-15 06:15:19 +0200

    add content

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 eve-log-alert
 =============
+
+This is a little tool to read a chat log file emitted by CCP's EVE Online
+client and sound an alert when certain phrases (or rather system names)
+are mentioned.
+
+It's a slightly more elaborate `tail -f ... | grep ... | notify-send`.  I
+couldn't get it to work as a shell pipeline mostly because `iconv` seemed
+to buffer too much, so here you go.
---
+++ alert.rb
@@ -0,0 +1,56 @@
+LOG_DIR     = "#{ENV['HOME']}/EVE/logs/Chatlogs"
+LOG_PREFIX  = "DEKCFC"
+
+require 'rb-inotify'
+
+Dir.chdir(File.dirname(__FILE__))
+
+latest, latest_mtime = nil
+
+Dir.foreach(LOG_DIR) do |file|
+  next unless file.start_with?(LOG_PREFIX)
+  file = "#{LOG_DIR}/#{file}"
+  mtime = File.mtime(file)
+  latest, latest_mtime = file, mtime if !latest || mtime > latest_mtime
+end
+
+raise "no matching log file found" unless latest
+
+filename = latest
+
+system_names = ARGV.map{|arg| /\b#{arg}/i}
+if system_names.empty?
+  raise "no system names given"
+end
+
+puts "reading #{filename}"
+last_alert_sound = 0
+File.open(filename, "rb:UTF-16LE") do |file|
+  file.seek(0, IO::SEEK_END)
+  notifier = INotify::Notifier.new
+  line_buf = ''
+  notifier.watch(filename, :modify, :close_write) do |event|
+    if event.flags.include?(:close_write)
+      notifier.stop
+    else
+      line_buf << file.read().encode("UTF-8")
+      *lines, line_buf = line_buf.split("\n", -1)
+      lines.delete_if { |line| !system_names.any? { |sys| sys.match(line)} }
+      lines.each do |line|
+        # "[ 2013.04.15 14:53:52 ] "
+        line.slice!(0, 24)
+      end
+      unless lines.empty?
+        system "notify-send", "#{LOG_PREFIX}: #{lines.join("\n")}"
+        now = Time.now.to_i
+        if now - last_alert_sound > 5
+          last_alert_sound = now
+          pid = Process.spawn("aplay", "alertsound",
+                              [:in, :out, :err] => :close)
+          Process.detach(pid)
+        end
+      end
+    end
+  end
+  notifier.run
+end
